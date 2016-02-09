385c09c4d9aa991b02fbe75afe88a80800c0304c
Vito Flavio Lorusso <vf.lorusso@live.com>
2016-02-09 17:00:51 +0100

    First skeleton version

---
+++ deploy-forge.ps1
@@ -0,0 +1,8 @@
+﻿Login-AzureRmAccount
+$resourcegroupName = "forge-deploy-test05"
+Select-AzureRmSubscription -SubscriptionName "vitolo-internal"
+New-AzureRmResourceGroup -Name $resourcegroupName -Location "West Europe"
+New-AzureRmResourceGroupDeployment -Name "forge-dev" `
+            -ResourceGroupName $resourcegroupName `
+            -TemplateFile "azuredeploy.json" `
+            -TemplateParameterFile "azuredeploy.parameters.json"
