2a826eee846604255690e04c33d636582d818f7c
Vito Flavio Lorusso <vf.lorusso@live.com>
2016-02-11 11:41:13 +0100

    added sample scripts

--- deploy-forge.ps1
+++ deploy-forge.ps1
@@ -1,8 +1,16 @@
 ﻿Login-AzureRmAccount
-$resourcegroupName = "forge-deploy-test05"
+$resourcegroupName = "forge-deploy-test06"
 Select-AzureRmSubscription -SubscriptionName "vitolo-internal"
 New-AzureRmResourceGroup -Name $resourcegroupName -Location "West Europe"
 New-AzureRmResourceGroupDeployment -Name "forge-dev" `
             -ResourceGroupName $resourcegroupName `
             -TemplateFile "azuredeploy.json" `
             -TemplateParameterFile "azuredeploy.parameters.json"
+New-AzureRmResourceGroupDeployment -Name "forge-dev-sb" `
+            -ResourceGroupName $resourcegroupName `
+            -TemplateFile "azuredeploy-sb.json" `
+            -TemplateParameterObject @{"namespaceName" = "vitotestsb"; "location" = "West Europe"}
+New-AzureRmResourceGroupDeployment -Name "forge-dev-classicvnet" `
+            -ResourceGroupName $resourcegroupName `
+            -TemplateFile "azuredeploy-classicnetwork.json" `
+            -TemplateParameterObject @{"vnetName" = "vitotestcvnet1"; "location" = "West Europe"}
